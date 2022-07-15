948eb4159b7aa54538426b03064a935c08126c05
leganderson-dev <61766198+leganderson-dev@users.noreply.github.com>
2022-07-15 13:44:24 +1000

    Create EastwindDBBackup.sql

---
+++ EastwindDBBackup.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [Eastwind] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\Eastwind.bkp' WITH NOFORMAT, INIT,  NAME = N'Eastwind-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
